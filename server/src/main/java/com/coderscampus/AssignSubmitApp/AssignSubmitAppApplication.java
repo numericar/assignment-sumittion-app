94e5e17fb259ac22b0fb016a46556da8e091abde
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-03-22 20:13:33 +0700

    initial projcet

---
+++ server/src/main/java/com/coderscampus/AssignSubmitApp/AssignSubmitAppApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.AssignSubmitApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignSubmitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignSubmitAppApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/coderscampus/AssignSubmitApp/AssignSubmitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.AssignSubmitApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignSubmitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
